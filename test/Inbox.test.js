6c68e8add282f673c3ee43b221f3c75197ede671
Olive105 <105.olivesoft@gmail.com>
2021-10-31 13:08:06 -0400

    feat - verifying the initial message

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,32 +2,31 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
 
-class Car {
-  park() {
-    return 'stopped';
-  }
+let accounts;
+let inbox;
 
-  drive() {
-    return 'vroom';
-  }
-}
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
 
-let car;
-
-beforeEach(() => {
-  car = new Car();
-  const printCar = () => {
-    console.log(car);
-  }
+  // Use one of those accounts to deploy the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode, 
+      arguments: ['Hi there!'],
+    })
+    .send({from: accounts[0], gas: '1000000'});
 });
 
-describe('Car', () => {
-  it('can park', () => {
-    assert.equal(car.park(), 'stopped');
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    assert.ok(inbox.options.address);
   });
 
-  it('can drive', () => {
-    assert.equal(car.drive(), 'vroom');
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi there!');
   });
 });
