086f497878297d6cc3c253d3fd7b78d894624119
Olive105 <105.olivesoft@gmail.com>
2021-10-31 21:13:46 -0400

    feat - Deployment to Rinkeby

--- deploy.js
+++ deploy.js
@@ -7,3 +7,15 @@ const provider = new HDWalletProvider(
   'https://rinkeby.infura.io/v3/247d5e920ce2491e81eb889bdf9baae6'
 );
 const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log('Attempting to deploy from account', accounts[0]);
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({data: bytecode, arguments: ['Hi there!']})
+    .send({from: accounts[0], gas: '1000000'});
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
