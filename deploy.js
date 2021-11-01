ec0ee715448a4d788f768385c1e1c51347bed685
Olive105 <105.olivesoft@gmail.com>
2021-11-01 10:33:09 -0400

    feat - Update to Solc v0.8.9

--- deploy.js
+++ deploy.js
@@ -1,19 +1,23 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
 
-const provider = new HDWalletProvider(
+const { abi, evm } = require('./compile');
+
+provider = new HDWalletProvider(
   'call grow acoustic vintage front ring trade assist shuffle mimic volume reject',
   'https://rinkeby.infura.io/v3/247d5e920ce2491e81eb889bdf9baae6'
 );
+
 const web3 = new Web3(provider);
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
+
   console.log('Attempting to deploy from account', accounts[0]);
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({data: bytecode, arguments: ['Hi there!']})
-    .send({from: accounts[0], gas: '1000000'});
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
 
   console.log('Contract deployed to', result.options.address);
   provider.engine.stop();
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,7 +2,8 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
-const { interface, bytecode } = require('../compile');
+
+const { abi, evm } = require('../compile');
 
 let accounts;
 let inbox;
@@ -10,23 +11,22 @@ let inbox;
 beforeEach(async () => {
   // Get a list of all accounts
   accounts = await web3.eth.getAccounts();
-
-  // Use one of those accounts to deploy the contract
-  inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({data: bytecode, arguments: ['Hi there!']})
-    .send({from: accounts[0], gas: '1000000'});
+  inbox = await new web3.eth.Contract(abi)
+    .deploy({
+      data: evm.bytecode.object,
+      arguments: ['Hi there!'],
+    })
+    .send({ from: accounts[0], gas: '1000000' });
 });
 
 describe('Inbox', () => {
   it('deploys a contract', () => {
     assert.ok(inbox.options.address);
   });
-
   it('has a default message', async () => {
     const message = await inbox.methods.message().call();
     assert.equal(message, 'Hi there!');
   });
-
   it('can change the message', async () => {
     await inbox.methods.setMessage('bye').send({ from: accounts[0] });
     const message = await inbox.methods.message().call();
